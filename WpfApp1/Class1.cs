05c9d13d4bec220ccc6464e8cdd976b572f61447
rayyang888 <113718595+rayyang888@users.noreply.github.com>
2022-11-04 10:18:54 -0400

    Initial commit

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class DiceInfo
+    {
+        public int Index { get; set; }
+        public int Dice1 { get; set; }
+        public int Dice2 { get; set; }
+    }
+
+    public static class Extensions
+    {
+        private static Random rnd = new Random();
+
+        public static void Shuffle<T>(this IList<T> values)
+        {
+            for (int i = values.Count - 1; i > 0; --i)
+            {
+                int k = rnd.Next(i + 1);
+                T val = values[k];
+                values[k] = values[i];
+                values[i] = val;
+            }
+        }
+    }
+
+    public class RollDice
+    {
+        private int _face = 0;
+        private int _factor = 0;
+        private int _numOfRolls = 0;
+
+        public RollDice(int face, int factor, int numOfRolls)
+        {
+            _face = face;
+            _factor = factor;
+            _numOfRolls = numOfRolls;
+        }
+
+        public List<DiceInfo> RollMyCice()
+        {
+            if (_numOfRolls == 0)
+                return null;
+
+            List<int> lstFaces = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            Random rnd = new Random();
+            List<DiceInfo> result = new List<DiceInfo>();
+
+            if (_face > 0)
+            {
+                for (int i = 0; i < _factor; ++i)
+                {
+                    lstFaces.Add(_face);
+                }
+            }
+
+            lstFaces.Shuffle();
+
+            for (int i = 1; i <= _numOfRolls; ++i)
+            {
+                int idx = rnd.Next(1, lstFaces.Count);
+                int d1 = lstFaces[idx];
+
+                idx = rnd.Next(1, lstFaces.Count);
+                int d2 = lstFaces[idx];
+
+                DiceInfo info = new DiceInfo { Index = i, Dice1 = d1, Dice2 = d2 };
+                result.Add(info);
+            }
+
+            return result;
+        }
+    }
+
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<DiceInfo> diceInfoList = new List<DiceInfo>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void rolledNum_Lost_Focus(object sender, RoutedEventArgs e)
+        {
+            string txt = rolledNum.Text.Trim();
+
+            int.TryParse(txt, out int result);
+
+            if (result <= 0)
+            {
+                MessageBox.Show("Please enter an integer");
+            }
+        }
+
+        private void factorSel_Initialized(object sender, EventArgs e)
+        {
+            factorSel.Items.Add("1");
+            factorSel.Items.Add("2"); 
+            factorSel.Items.Add("3"); 
+            factorSel.Items.Add("4");
+            factorSel.Items.Add("5");
+        }
+
+        private void faceSel_Initialized(object sender, EventArgs e)
+        {
+            faceSel.Items.Add("1");
+            faceSel.Items.Add("2");            
+            faceSel.Items.Add("3");
+            faceSel.Items.Add("4");
+            faceSel.Items.Add("5");
+            faceSel.Items.Add("6");
+        }
+
+        private void rollDice_Click(object sender, RoutedEventArgs e)
+        {
+            var face = (string)faceSel.SelectedItem;
+
+            if (string.IsNullOrEmpty(face))
+            {
+                MessageBox.Show("Please select a face of dice.");
+                return;
+            }
+
+            var factor = (string)factorSel.SelectedItem;
+
+            if (string.IsNullOrEmpty(factor))
+            {
+                MessageBox.Show("Please select a factor.");
+                return;
+            }
+
+            var rnum = rolledNum.Text;
+
+            if (string.IsNullOrWhiteSpace(rnum))
+            {
+                MessageBox.Show("Please enter number of times dice will be rolled.");
+                return;
+            }
+
+            if (!int.TryParse(rnum, out int irnum))
+            {
+                MessageBox.Show("Please enter a integer for number of times dice will be rolled.");
+                return;
+            }
+
+            int iface = int.Parse(face);
+            int ifactor = int.Parse(factor);
+
+            RollDice rollDice = new RollDice(iface, ifactor, irnum);
+
+            List<DiceInfo> lstDiceInfo = rollDice.RollMyCice();
+            diceInfoList.AddRange(lstDiceInfo);
+
+            lvHistory.ItemsSource = null;
+            lvHistory.Items.Clear();
+            lvHistory.ItemsSource = diceInfoList;
+            lvHistory.SelectedIndex = 0;
+            lvHistory.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lvHistory.ItemsSource = null;
+            lvHistory.Items.Clear();
+            diceInfoList.Clear();
+        }
+    }
+}
